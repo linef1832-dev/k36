f392833ab3c90b3d10335ab4348368801d41c428
linef1832-dev <linef1832@gmail.com>
2026-04-10 15:22:11 +0700

    Update global.js

--- js/global.js
+++ js/global.js
@@ -104,6 +104,9 @@ async function showPage(pageName) {
                         }
                     }
                 }
+                else if (pageName === 'fine') {
+                    if (typeof initFineApp === 'function') await initFineApp();
+                }
                 else if (pageName === 'leave') {
                     if (typeof initLeaveTable === 'function') await initLeaveTable();
                 }
